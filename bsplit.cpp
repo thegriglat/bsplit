d219ce827e140edf4186234a7781e26e8af62d07
Grigorii Latyshev <grigorii.latyshev@cern.ch>
2015-12-13 20:33:33 +0100

    Added Autosave + skip BJet check

--- bsplit.cpp
+++ bsplit.cpp
@@ -42,7 +42,6 @@ int main(int argv, char **argc){
   random_device _randomDevice;
   mt19937 _generator (_randomDevice());
   uniform_real_distribution<> _uniformDist (0, 1);
-
   for (size_t i = 0; i <  dirs->GetEntries(); i++){
     TTree *t = (TTree*)dirs->At(i);
     t = (TTree*)fin->Get(t->GetName());
@@ -55,6 +54,8 @@ int main(int argv, char **argc){
     TTree* newt0 = t->CloneTree(0);
     outfiles[1]->cd();
     TTree* newt1 = t->CloneTree(0);
+    newt1->SetAutoSave(10000);
+    newt0->SetAutoSave(10000);
     for (int eidx = 0; eidx < tentries; eidx++){
       t->GetEntry(eidx);
       if (_uniformDist(_generator) < trainperc){
@@ -65,31 +66,32 @@ int main(int argv, char **argc){
     }
     newt0->Write();
     newt1->Write();
-    newt0 = 0;
-    newt1 = 0;
+//    newt0 = 0;
+//    newt1 = 0;
   }
 
   // Verifying
-  cout << "Verifying by sum of SingleTop_1__BJet_1__Pt branch values ..." << endl;
+//  cout << "Verifying by sum of SingleTop_1__BJet_1__Pt branch values ..." << endl;
   for (size_t i = 0; i <  dirs->GetEntries(); i++){
     TTree *t = (TTree*)dirs->At(i);
     t = (TTree*)fin->Get(t->GetName());
-    double sumLS = 0;
+    //double sumLS = 0;
     int trueEntries = t->GetEntries();
     int sumEntries = 0;
-    double insumLS = getSumLS(fin, t->GetName());
-    cout << "Sum of 'SingleTop_1__BJet_1__Pt' in tree " << t->GetName() << " = " << insumLS  << endl; 
+    //double insumLS = getSumLS(fin, t->GetName());
+    //cout << "Sum of 'SingleTop_1__BJet_1__Pt' in tree " << t->GetName() << " = " << insumLS  << endl; 
     for (int fid = 0; fid < outfiles.size(); fid++){
-      double LS = getSumLS(outfiles[fid], t->GetName());
-      cout << "  File: ." << fid << "| Sum of 'SingleTop_1__BJet_1__Pt' in tree " << t->GetName() << " = " << LS << endl; 
-      sumLS += LS;
+      //double LS = getSumLS(outfiles[fid], t->GetName());
+      //cout << "  File: ." << fid << "| Sum of 'SingleTop_1__BJet_1__Pt' in tree " << t->GetName() << " = " << LS << endl; 
+      //sumLS += LS;
       sumEntries += ((TTree*)outfiles[fid]->Get(t->GetName()))->GetEntries();
     }
-    if (sumLS == insumLS and trueEntries == sumEntries){
-      cout << "==> OK. Sum of SingleTop_1__BJet_1__Pt is equal." << endl;
+    //if (sumLS == insumLS and trueEntries == sumEntries){
+    if (trueEntries == sumEntries){
+//      cout << "==> OK. Sum of SingleTop_1__BJet_1__Pt is equal." << endl;
       cout << "==> Entries " << trueEntries << " == " << sumEntries << endl;
     } else{
-      cout << "==> Something wrong. " << insumLS << " != " << sumLS << endl;
+  //    cout << "==> Something wrong. " << insumLS << " != " << sumLS << endl;
       cout << "==> Entries " << trueEntries << " != " << sumEntries << endl;
       cout << "Exiting ..." << endl;
       fin->Close();
